a02efe98a025aeb1d7ae6525703dc7e904e3e269
Chittle Skuny <chittleskuny@gmail.com>
2021-11-12 17:24:19 +0800

    Make resizer be a class

--- python/resizer.py
+++ python/resizer.py
@@ -5,18 +5,35 @@ from PIL import Image
 python3 resizer infile width*height
 '''
 
+class Resizer(object):
+    def __init__(self):
+        pass
 
-def get_outfile(infile):
-    root, ext = os.path.splitext(infile)
-    outfile = '{}_out{}'.format(root, ext)
-    return outfile
+    def get_outfile(self, infile, width, height):
+        root, ext = os.path.splitext(infile)
+        # Image.NEAREST (0), Image.LANCZOS (1), Image.BILINEAR (2), Image.BICUBIC (3), Image.BOX (4) or Image.HAMMING (5)
+        outfile = Image.open(infile).resize((width, height), Image.NEAREST)
+        outfile.save('{}_out{}'.format(root, ext))
+        return outfile
 
+    def resize_image(self, infile_or_indir, width_height):
+        (width, height) = width_height.split('*')
+        (width, height) = (int(width), int(height))
+        if os.path.isfile(infile_or_indir):
+            infile = infile_or_indir
+            self.get_outfile(infile, width, height)
+        elif os.path.isdir(infile_or_indir):
+            indir = infile_or_indir
+            for root, dirs, files in os.walk(indir):
+                for file in files:
+                    infile = os.path.join(root, file)
+                    self.get_outfile(infile, width, height)
+        else:
+            return 1
 
-def resize_image(infile, width_height):
-    (width, height) = width_height.split('*')
-    out = Image.open(infile).resize((int(width), int(height)), Image.ANTIALIAS)
-    out.save(get_outfile(infile))
+        return 0
 
 
 if __name__ == '__main__':
-    resize_image(sys.argv[1], sys.argv[2])
+    r = Resizer()
+    r.resize_image(sys.argv[1], sys.argv[2])
